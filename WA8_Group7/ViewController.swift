c0e3603cd649e99ba0c2dadcfce6d49e7ee5976b
Tianshu Yang <yts9614@gmail.com>
2024-11-09 14:26:54 -0800

    Initial Commit

---
+++ WA8_Group7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WA8_Group7
+//
+//  Created by 杨天舒 on 11/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
